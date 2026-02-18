7b7010f16a13f5393f387c72ee9b2268b5deca42
nileshdesai87 <nileshdsai87@gmail.com>
2026-02-18 17:24:47 +0530

    Remove duplicate export class

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -15,17 +15,3 @@ class LoginPage {
 }
 
 export default LoginPage;
-export default class LoginPage {
-  constructor(page) {
-    this.page = page;
-    this.username = page.locator('#username');
-    this.password = page.locator('#password');
-    this.submit = page.locator('#submit');
-  }
-
-  async login(user, pass) {
-    await this.username.fill(user);
-    await this.password.fill(pass);
-    await this.submit.click();
-  }
-}
